0451760aa3170e1581968659da1b1231fba44110
Werneck <werneckcolins@outlook.com>
2025-04-10 02:42:58 -0300

    Iniciando nível Mestre

--- jogo_trunfo_c/main.c
+++ jogo_trunfo_c/main.c
@@ -17,6 +17,13 @@ float pib_pc2(float pib_2, float n_populacao_2){
     return pib_2 / n_populacao_2;
 }
 
+float soma_power1(float n_populacao_1, float area_1, float pib_1, int np_turisticos_1, float result_dp, float result_pib_pc){
+    return n_populacao_1 + area_1 + pib_1 + np_turisticos_1 + result_dp + result_pib_pc;
+}
+
+float soma_power2(float n_populacao_2, float area_2, float pib_2, int np_turisticos_2, float result_dp2, float result_pib_pc2){
+    return n_populacao_2 + area_2 + pib_2 + np_turisticos_2 + result_dp2 + result_pib_pc2;
+}
 
 int main (){
 
@@ -27,7 +34,7 @@ int main (){
 // Área
 // PIB
 // Número de Pontos Turísticos
-
+    // int comparacao;
     char estado_1, estado_2;
     char id_1[4], id_2[4];
     // char id[32][4];
@@ -66,11 +73,14 @@ int main (){
     printf("Numero de pontos turisticos: ");
     scanf(" %d", &np_turisticos_1);
 // Chamando a Função de Densidade populacional Carta 1
-    float result = densidade_p(n_populacao_1, area_1);
-    printf("Densidade Populacional: %f hab/km\n", result);
+    float result_dp = densidade_p(n_populacao_1, area_1);
+    printf("Densidade Populacional: %f hab/km\n", result_dp);
 // Chamando a Função de PIB per Capita Carta 1
     float result_pib_pc = pib_pc(pib_1, n_populacao_1);
     printf("PIB per Capita: %f reais\n", result_pib_pc);
+// Super Poder Carta 1
+    float super_poderC1 = soma_power1(n_populacao_1,area_1,pib_1,np_turisticos_1,result_dp,result_pib_pc);
+    printf("Super Poder: %.3f", super_poderC1);
 
     printf("\n*___________________________________________________*\n");
     printf("\nAgora digite os valores de sua segunda carta!!\n");
@@ -97,11 +107,15 @@ int main (){
     printf("Numero de pontos turisticos: ");
     scanf(" %d", &np_turisticos_2);
 // Chamando a Função de Densidade populacional Carta 2
-    float result_2 = densidade_p(n_populacao_2, area_2);
-    printf("Densidade Populacional: %f hab/km\n", result_2);
+    float result_dp2 = densidade_p(n_populacao_2, area_2);
+    printf("Densidade Populacional: %f hab/km\n", result_dp2);
 // Chamando a Função de PIB per Capita Carta 2
     float result_pib_pc2 = pib_pc2(pib_2, n_populacao_2);
     printf("PIB per Capita: %f reais\n", result_pib_pc2);
+// Super Poder Carta 2
+    float super_poderC2 = soma_power2(n_populacao_2,area_2,pib_2,np_turisticos_2,result_dp2,result_pib_pc2);
+    printf("Super Poder: %.3f", super_poderC2);
+
 
     printf("\n***************************************************************************************\n");
     printf("Que otimas cartas! Agora vou exibi-las respectivamente para voce ver todo seu poder!!!\n");
@@ -116,8 +130,9 @@ int main (){
     printf("Area: %f km\n", area_1);
     printf("PIB: %f bilhoes de reais\n", pib_1);
     printf("Numero de Pontos Turisticos: %d\n", np_turisticos_1);
-    printf("Densidade Populacional: %.2f hab/km\n", result);
+    printf("Densidade Populacional: %.2f hab/km\n", result_dp);
     printf("PIB per Capita: %f reais\n", result_pib_pc);
+    printf("Super Poder: %.3f", super_poderC1);
 
     printf("\nCarta 2 \n");
 
@@ -128,8 +143,15 @@ int main (){
     printf("Area: %f km\n", area_2);
     printf("PIB: %f bilhoes de reais\n", pib_2);
     printf("Numero de Pontos Turisticos: %d\n", np_turisticos_2);
-    printf("Densidade Populacional: %f hab/km\n", result_2);
+    printf("Densidade Populacional: %f hab/km\n", result_dp2);
     printf("PIB per Capita: %f reais\n", result_pib_pc2);
+    printf("Super Poder: %.3f\n", super_poderC2);
+
+    if(n_populacao_1 > n_populacao_2){
+        printf("Carta 1 vence com %.2f pontos em populacao", n_populacao_1);
+    } else{
+        printf("Carta 2 vence com %.2f pontos em populacao", n_populacao_2);
+    }
 
     system("pause");
     return 0;
